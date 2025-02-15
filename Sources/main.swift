846ae3f39649aafb0d5b571368eb56c4dfd37761
4ster-light <davidvivarbogonez@gmail.com>
2025-02-15 18:03:03 +0100

    Reformatted code

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,24 +3,24 @@ import Foundation
 import Rainbow
 
 struct Stats: ParsableCommand {
-    @Argument(help: "Directory to analyze. Defaults to current directory.")
-    var directory: String = "."
+  @Argument(help: "Directory to analyze. Defaults to current directory.")
+  var directory: String = "."
 
-    func run() throws {
-        let directoryURL = URL(fileURLWithPath: directory, isDirectory: true)
+  func run() throws {
+    let directoryURL = URL(fileURLWithPath: directory, isDirectory: true)
 
-        let result = getFiles(directory: directoryURL)
-            .flatMap { files in
-                let analyses = files.compactMap { analyzeFile(filePath: $0) }
-                return .success(analyses)
-            }
-            .map(aggregateResults)
-            .map(displayResults)
+    let result = getFiles(directory: directoryURL)
+      .flatMap { files in
+        let analyses = files.compactMap { analyzeFile(filePath: $0) }
+        return .success(analyses)
+      }
+      .map(aggregateResults)
+      .map(displayResults)
 
-        if case .failure(let error) = result {
-            print(error.description.red)
-        }
+    if case .failure(let error) = result {
+      print(error.description.red)
     }
+  }
 }
 
 Stats.main()
